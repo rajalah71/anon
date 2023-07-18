a8ecb1c7f4a57f77fe7b53131bd331d17345fba4
rajalah71 <rajalah71@gmail.com>
2023-07-18 15:20:56 +0300

    added a cryptographic tool

---
+++ R/crypto.r
@@ -0,0 +1,107 @@
+#' Encrypts a message using RSA encryption.
+#'
+#' This function takes a message as input and encrypts it using the RSA encryption
+#' algorithm with the provided parameters.
+#'
+#' @param m The message to be encrypted.
+#' @param e The public exponent used for encryption.
+#' @param n The modulus used for encryption.
+#' @param bits The number of bits used for precision in the encrypted message.
+#'
+#' @return A mpfr list representing the encrypted message.
+#' @importFrom Rmpfr mpfr
+#' @importFrom openssl bignum bignum_mod_exp
+encrypt_message = function(m, e, n, bits) {
+
+  # Convert each character of the message to raw
+  m = lapply(as.character(m), charToRaw)
+
+  # Convert each raw character to a bignum
+  message_bignum <- lapply(m, bignum)
+
+  # Encrypt each bignum using bignum_mod_exp function with given parameters e and n
+  encrypted_message <- lapply(message_bignum, function(x) bignum_mod_exp(x, e, n))
+
+  # read the encrypted values to
+  as_character = lapply(encrypted_message, as.character)
+  as_numerical = mpfr(unlist(as_character), bits)
+
+  # Return the encrypted message
+  return(as_numerical)
+}
+
+
+
+#' Encrypts the data using RSA encryption and scales the encrypted values to [0, 1].
+#'
+#' This function takes a data frame as input, encrypts each column using RSA encryption
+#' with the provided number of bits and scales the encrypted values to the [0, 1] range.
+#'
+#' @param data The data frame to be encrypted and scaled.
+#' @param bits The number of bits used for precision in the RSA encryption.
+#'
+#' @return A scaled data frame where each column contains the encrypted and scaled values.
+#'
+#' @importFrom openssl rsa_keygen
+#' @importFrom Rmpfr mpfr
+#'
+#' @examples
+#' data <- data.frame(x = c(10, 20, 30), y = c(5, 15, 25))
+#' encrypted_data <- encrypt(data, bits = 2048)
+#' print(encrypted_data)
+#'
+#' @export
+encrypt <- function(data, bits = 2048) {
+  # Step 4.1: Generate RSA keys
+  key = rsa_keygen(bits)
+  pubkey = as.list(key)$pubkey
+
+  # parameters for rsa
+  e <- pubkey$data$e
+  n <- pubkey$data$n
+
+  # Encrypt the data
+  encrypted = lapply(data, function(x) encrypt_message(x, e, n, bits))
+
+
+  # Make an empty dataframe of the original size to store the minmax scaled values
+  col = ncol(data)
+  row = nrow(data)
+  scaled = data.frame(matrix(nrow = row, ncol = col))
+  colnames(scaled) = colnames(data)
+
+  uniques_enc = c()
+
+  for(i in seq_len(col)){
+    min = min(encrypted[[i]])
+    max = max(encrypted[[i]])
+    diff = max - min
+
+    uniques_enc[i] = length(unique(encrypted[[i]]))
+
+    # print(encrypted[[i]])
+
+    for(j in seq_len(row)){
+      scaled[j,i] = as.double((encrypted[[i]][[j]] - min) / diff)
+    }
+  }
+
+  uniques_double = sapply(scaled, function(x) length(unique(x)))
+
+  geometric_mean_result <- prod(uniques_double / uniques_enc)^(1/length(uniques_double / uniques_enc))
+
+  cat("Information loss on conversion to double:",1-geometric_mean_result,"\n")
+
+  return(scaled)
+
+}
+
+# encrypt(data_frame)
+#
+# data3 <- data.frame(
+#   age = c(1, 1, 1, 2, 2),
+#   gender = c(2, 2, 2, 3, 3),
+#   zip_code = c(4, 4, 4, 5, 5),
+#   disease = c(4,5,5,6, 6)
+# )
+
--- wip/testi.r
+++ wip/testi.r
@@ -1,33 +1,38 @@
-onehot_encode_my <- function(data) {
-  # Identify columns to one-hot encode
-  columns_to_encode <- data %>%
-    select_if(is.character) %>%
-    colnames
+# function to combine values in ordered categorical variable column
 
-  columns_to_encode <- which(colnames(data) %in% columns_to_encode)
+combine_ordered = function(column){
 
-  print(columns_to_encode)
+  # get unique values in the column and sort them
+  unique_values = unique(column)
+  sorted_uniques = sort(unique_values)
+  #count the number of unique values
+  n = length(unique_values)
 
-  if (length(columns_to_encode) == 0) {
-    # No columns to encode
-    return(data)
+  # check whether the the amount of unique values is even or odd
+  if(n %% 2 == 0){
+
+    # make bands of sorted unique values as [sorted_uniques[i], sorted_uniques[i+1]] for all i in [1,3,5,...,n-1]
+    for(i in seq(1, n-1, 2)){
+      column[column == sorted_uniques[i]] = paste0("[", sorted_uniques[i], ",", sorted_uniques[i+1], "]")
+      column[column == sorted_uniques[i+1]] = paste0("[", sorted_uniques[i], ",", sorted_uniques[i+1], "]")
+    }
+  } else {
+    # Take the middle value of the sorted unique values and make it a bands of its ows while the rest of the values are banded as in the even case
+    middle = sorted_uniques[(n+1)/2]
+    #column[column == middle] = paste0("[", middle, ",", middle, "]")
+
+    for(i in seq(1, middle-2, 2)){
+      column[column == sorted_uniques[i]] = paste0("[", sorted_uniques[i], ",", sorted_uniques[i+1], "]")
+      column[column == sorted_uniques[i+1]] = paste0("[", sorted_uniques[i], ",", sorted_uniques[i+1], "]")
+    }
+
+    for(i in seq(middle+1, n-1 , 2)){
+      column[column == sorted_uniques[i]] = paste0("[", sorted_uniques[i], ",", sorted_uniques[i+1], "]")
+      column[column == sorted_uniques[i+1]] = paste0("[", sorted_uniques[i], ",", sorted_uniques[i+1], "]")
+    }
   }
 
-  # One-hot encode each column
-  for (col_index in columns_to_encode) {
-    col_name <- names(data)[col_index]
-    encoded_cols <- model.matrix(~ . - 1, data = data[, col_index, drop = FALSE])
-    col_names <- colnames(encoded_cols)
+  return(column)
 
-    # Remove original column
-    data <- data[, -col_index, drop = FALSE,]
 
-    # Append encoded columns to dataframe
-    data <- cbind(data, encoded_cols)
-
-    # Rename encoded columns
-    colnames(data)[(ncol(data) - length(col_names) + 1):ncol(data)] <- paste(col_name, col_names, sep = ".")
   }
-
-  return(data)
-}
