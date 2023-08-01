3037b67580cd0b13ad2c2053bc4a005b17ba8bf9
Johannes <jtraja@utu.fi>
2023-08-01 11:04:20 +0300

    Added namespaces to anonymity measures

--- R/anon_measures.r
+++ R/anon_measures.r
@@ -65,7 +65,7 @@ distances = function(original_data, reference_data = NULL, dist = euc_dist){
   if(!is.null(reference_data)) reference_data = scale(reference_data)
 
   # calculate the distance between each row of the original data against every row of the reference data and return the sorted for each row
-  # if cross_val is TRUE, the distance is calculated by leaving one row out of the original data and calculating the distance between the left out row and the rest of the original data
+  # if reference_data is null, the distance is calculated by leaving one row out of the original data and calculating the distance between the left out row and the rest of the original data
 
   if(is.null(reference_data)){
     sorted_distances = as.data.frame(matrix(NA, nrow(original_data), nrow(original_data)-1))
@@ -93,8 +93,18 @@ distances = function(original_data, reference_data = NULL, dist = euc_dist){
 
 #-----------------------------------------------------------------
 
-
-prediction_distance = function(distances){
+#' Prediction Distance
+#'
+#' Calculate distances between each row of the original_data and the reference_data using the specified distance function.
+#'
+#' @param original_data The original dataset.
+#' @param reference_data The reference dataset (default: NULL).
+#' @param dist Distance function to use (default: euc_dist).
+#' @return A numeric vector containing the distances between each row of the original_data and the reference_data.
+#' @export
+prediction_distance = function(original_data, reference_data = NULL, dist = euc_dist){
+  # calculate distances
+  distances = distances(original_data, reference_data, dist)
   # return the first column of the distances
   return(distances[,1])
 }
@@ -103,9 +113,25 @@ prediction_distance = function(distances){
 
 #-----------------------------------------------------------------
 
-
-prediction_ambiguity = function(distances, k){
+#' Prediction Ambiguity
+#'
+#' Calculates the prediction ambiguity by comparing the relative distance from each record to its nearest neighbor versus its kth nearest neighbor in the set of distances.
+#'
+#' @param original_data The original dataset for which prediction ambiguity needs to be calculated.
+#' @param k The number of nearest neighbors to consider for calculating the ambiguity.
+#' @param reference_data (Optional) The reference dataset used for calculating the distances. If not provided, the distances will be computed within the original_data.
+#' @param dist The distance function to be used. Default is euclidean distance (euc_dist).
+#'
+#' @return A numeric vector representing the prediction ambiguity for each record in the original_data.
+#'
+#' @examples
+#' prediction_ambiguity(original_data = iris, k = 3)
+#'
+#'
+#' @export
+prediction_ambiguity = function(original_data, k, reference_data = NULL, dist = euc_dist){
   # Prediction ambiguity gives the relative distance from the record Aj to the nearest versus the kth-nearest record in the set distances
+  distances = distances(original_data, reference_data, dist)
   # return the first column of the distances divided by the kth column of the distances
   return(distances[,1]/distances[,k])
 }
@@ -113,23 +139,87 @@ prediction_ambiguity = function(distances, k){
 
 #-----------------------------------------------------------------
 
-prediction_ambiguity = function(distances, k){
-  # Prediction ambiguity gives the relative distance from the record Aj to the nearest versus the kth-nearest record in the set distances
-  # return the first column of the distances divided by the kth column of the distances
-  return(distances[,1]/distances[,k])
-}
 
-prediction_uncertainty = function(original_data, reference_data, k, dist = euc_dist){
+#' Prediction Uncertainty
+#'
+#' Prediction uncertainty gives the variation among the k best matches for each row of the original data in the reference data for each row of the original data, find the k best matches in the reference data using the dist function. If the reference data is null, the function performs leave-one-out cross-validation to calculate the distance between each row of the original data against the rest of the original data. Onehot encode the datasets to enable distance calculations. Only run the numeric version if it is not null.
+#'
+#' @param original_data The original dataset.
+#' @param reference_data The reference dataset of the same column space (default: NULL).
+#' @param k Number of best matches to find.
+#' @param dist Distance function to use (default: euc_dist).
+#' @return A numeric vector containing the variances for each row of the original_data.
+#' @export
+#'
+#' @examples
+#' prediction_uncertainty(original_data = iris, k = 3)
+#'
+#' @importFrom onehot onehot
+prediction_uncertainty = function(original_data, k, reference_data = NULL, dist = euc_dist){
   # Prediction uncertainty gives the variation among the k best matches for each row of the original data in the reference data
   # for each row of the original data, find the k best matches in the reference data using the dist function
 
-  # onehot encode the datasets to enable distance calculations
+  # If the reference data is null, the function performs leave-one-out cross-validation to calculate the distance between each row of the original data against the rest of the original data
+
+  # onehot encode the datasets to enable distance calculations. Only run the numeric version if it is not null
   numeric_original = predict(onehot(original_data, stringsAsFactors = TRUE, max_levels = Inf), original_data)
-  numeric_reference = predict(onehot(reference_data, stringsAsFactors = TRUE, max_levels = Inf), reference_data)
+  if(!is.null(reference_data)) numeric_reference = predict(onehot(reference_data, stringsAsFactors = TRUE, max_levels = Inf), reference_data)
+
+  # scale both datasets
+  numeric_original = scale(numeric_original)
+  if(!is.null(reference_data)) numeric_reference = scale(numeric_reference)
+
+  # empty vector to store the variances
+  variances = rep_len(NA, nrow(numeric_original))
+
+  # find the k nearest rows in the reference data for each row of the original data. If the reference data is null, the function performs leave-one-out cross-validation to calculate the distance between each row of the original data against the rest of the original data
+  if(!is.null(reference_data)){
+    for(i in seq(nrow(numeric_original))){
+      k_nearest = as.data.frame(matrix(NA, k, ncol(numeric_original)))
+
+      # add rows of the reference data to the k_nearest dataframe if they are closer than the current furthest row
+      for(j in seq(nrow(numeric_reference))){
+        if(j <= k){
+          k_nearest[j,] = numeric_reference[j,]
+        } else {
+          if(dist(numeric_original[i,], numeric_reference[j,]) < max(distances(k_nearest, numeric_original[i,], dist = dist))){
+            k_nearest[which.max(distances(k_nearest, numeric_original[i,], dist = dist)),] = numeric_reference[j,]
+          }
+        }
+      }
+
+      # calculate the variance of the columns of the k nearest rows and take the mean of them
+      variances[i] = mean(apply(k_nearest, 2, var))
+
+    }
+  } else{
+    for(i in seq(nrow(numeric_original))){
+      k_nearest = as.data.frame(matrix(NA, k, ncol(numeric_original)))
+
+      # add rows of the reference data to the k_nearest dataframe if they are closer than the current furthest row
+      for(j in seq(nrow(numeric_original))){
+        # skip the row if it is the same as the row of the original data
+        if(j == i) next
+
+        if(anyNA(k_nearest)){
+          k_nearest[j,] = numeric_original[j,]
+        } else {
+          if(dist(numeric_original[i,], numeric_original[j,]) < max(distances(k_nearest, numeric_original[i,], dist = dist))){
+            k_nearest[which.max(distances(k_nearest, numeric_original[i,], dist = dist)),] = numeric_original[j,]
+          }
+        }
+      }
+
+      # calculate the variance of the columns of the k nearest rows and take the mean of them
+      variances[i] = mean(apply(k_nearest, 2, var))
+
+    }
+  }
 
 
-
-  sorted_by_distance = emptydf()
+  return(variances)
 
 
 }
+
+
